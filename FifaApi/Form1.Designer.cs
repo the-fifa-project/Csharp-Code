fdf1a49f471ce189395f598d74c654ff871658c8
Donito <donitor@live.nl>
2019-05-06 11:54:01 +0200

    Fifa application c#

---
+++ FifaApi/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace FifaApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getDataButton = new System.Windows.Forms.Button();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.idLabel = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.inputNumber = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.inputNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // getDataButton
+            // 
+            this.getDataButton.Location = new System.Drawing.Point(118, 32);
+            this.getDataButton.Name = "getDataButton";
+            this.getDataButton.Size = new System.Drawing.Size(117, 36);
+            this.getDataButton.TabIndex = 0;
+            this.getDataButton.Text = "Get Team Data";
+            this.getDataButton.UseVisualStyleBackColor = true;
+            this.getDataButton.Click += new System.EventHandler(this.getDataButton_Click);
+            // 
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(95, 113);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(59, 20);
+            this.nameLabel.TabIndex = 2;
+            this.nameLabel.Text = "label1";
+            this.nameLabel.Click += new System.EventHandler(this.nameLabel_Click);
+            // 
+            // idLabel
+            // 
+            this.idLabel.AutoSize = true;
+            this.idLabel.Location = new System.Drawing.Point(12, 284);
+            this.idLabel.Name = "idLabel";
+            this.idLabel.Size = new System.Drawing.Size(46, 17);
+            this.idLabel.TabIndex = 3;
+            this.idLabel.Text = "label2";
+            this.idLabel.Click += new System.EventHandler(this.idLabel_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 16;
+            this.listBox.Location = new System.Drawing.Point(88, 136);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(229, 36);
+            this.listBox.TabIndex = 7;
+            // 
+            // inputNumber
+            // 
+            this.inputNumber.Location = new System.Drawing.Point(12, 40);
+            this.inputNumber.Maximum = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            this.inputNumber.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.inputNumber.Name = "inputNumber";
+            this.inputNumber.Size = new System.Drawing.Size(100, 22);
+            this.inputNumber.TabIndex = 8;
+            this.inputNumber.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Members:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Select a team!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(503, 310);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.inputNumber);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.idLabel);
+            this.Controls.Add(this.nameLabel);
+            this.Controls.Add(this.getDataButton);
+            this.Name = "Form1";
+            this.Text = "FIFA GAMBLE APPLICATION";
+            ((System.ComponentModel.ISupportInitialize)(this.inputNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getDataButton;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label idLabel;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.NumericUpDown inputNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
