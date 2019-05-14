c330644806ca663ee8c3758c7595877b61c15658
Donito <donitor@live.nl>
2019-05-14 08:56:18 +0200

    Api V3

---
+++ FifaApi V3/FifaApi/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace FifaApi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getDataButton_Click(object sender, EventArgs e)
+        {
+            listBox.Items.Clear();
+            int input = (int)inputNumber.Value;
+            // Bron: https://stackoverflow.com/a/4758334
+            System.Net.WebClient downloader = new System.Net.WebClient();
+            string dataJson;
+
+            dataJson = downloader.DownloadString("http://localhost/the_fifa_project/api/?id=" + input.ToString());
+
+            
+            Data[] data = JsonConvert.DeserializeObject<Data[]>(dataJson);
+            
+            nameLabel.Text = "Team name: " + data[0].name;
+            idLabel.Text = "" + data[0].id;
+            label6.Text = "" + data[0].goals;
+            label7.Text = "" + data[0].wins;
+            label8.Text = "" + data[0].loses;
+        }
+
+        private void idLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void nameLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
